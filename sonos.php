868a4631f7f8efe063e475c14b272bc9311d7e20
Mark van Eijk <mark@vormkracht10.nl>
2024-02-15 10:30:06 +0100

    Update sonos.php

--- sonos.php
+++ sonos.php
@@ -9,122 +9,55 @@ use Illuminate\Support\Str;
 
 class Sonos
 {
-    public $controllers;
-
-    public $results;
-
-    public $apiKey = '';
-
-    //Dit moet ook de refresh time van de deamon worden om het php script te runnen
-    public $expiration = 600;
+    public $network;
 
     public $endpoint = 'https://vormkracht10-app.test/sonos/controller';
 
 
-    public function __construct()
-    {
-        $network = new Network();
-        $this->controllers = $network->getControllers();
-    }
+    public function __construct(Network $network) {}
 
     public function run()
     {
-        $timezone = 'Europe/Amsterdam';
-        $this->results = [];
+        $tracks = $this->getNowPlayingTracks();
+        
+        $this->sendTracksToWebhooks($tracks);
+    }
 
-        foreach ($this->controllers as $controller) {
+    public function getNowPlayingTracks()
+    {
+        $tracks = [];
+        
+        foreach ($this->network->getControllers() as $controller) {
             $stateDetails = $controller->getStateDetails();
-            $result = [
+            
+            $tracks = [
                 'speaker' => $controller->getRoom(),
                 'title' => $stateDetails->getTitle(),
                 'artist' => $stateDetails->getArtist(),
-                'token' => hash('sha256', Carbon::now($timezone)->toDateTimeString()),
-                'date' => Carbon::now($timezone)->toDateTimeString(),
+                'cover' => $stateDetails->getAlbumArt() ? base64_encode(file_get_contents($stateDetails->getAlbumArt())) : null,
+                'timestamp' => time(),
             ];
-
-            $albumArtUrl = $stateDetails->getAlbumArt();
-
-            if (!empty($albumArtUrl)) {
-                $fileName = Str::random(40);
-                $destinationPathWithExtension = "images/{$fileName}.jpg";
-
-                if (!file_exists('images')) {
-                    mkdir('images');
-                }
-
-                $imageContent = file_get_contents($albumArtUrl);
-                file_put_contents($destinationPathWithExtension, $imageContent);
-
-                $imageUrl = $this->UploadImage($destinationPathWithExtension);
-
-                if ($imageUrl) {
-                    $result['imageFilePath'] = $imageUrl;
-                } else {
-                    $result['imageFilePath'] = null;
-                }
-            } else {
-                $result['imageFilePath'] = null;
-            }
-
-            $this->results[] = $result;
         }
+
+        return $tracks;
     }
 
-    public function UploadImage($imagePath)
-    {
-
-        $params = [
-            'expiration' => $this->expiration,
-            'key' => $this->apiKey,
-        ];
-
-        $client = new Client();
-
-        try {
-            $response = $client->post('https://api.imgbb.com/1/upload', [
-                'multipart' => [
-                    [
-                        'name' => 'image',
-                        'contents' => fopen($imagePath, 'r'),
-                    ],
-                ],
-                'query' => $params,
-            ]);
-
-            $responseData = json_decode($response->getBody(), true);
-
-            if (isset($responseData['data']['url'])) {
-                return $responseData['data']['url'];
-            }
-
-            return null;
-        } catch (\Exception $e) {
-            echo 'Error uploading image: ' . $e->getMessage();
-
-            return null;
-        }
-    }
-
-    public function __destruct()
+    public function sendTracksToWebhooks(array $tracks)
     {
         $endpoints = [
-            'https://vormkracht10-app.test/sonos/webhook',
-            'https://vormkracht10.app/sonos/webhook',
+            'https://vormkracht10-app.test/webhooks/sonos,
+            'https://vormkracht10.app/webhooks/sonos',
         ];
 
         $client = new Client();
 
-        foreach ($endpoints as $endpoint) {
-            try {
-                $response = $client->post($endpoint, [
-                    'form_params' => ['results' => json_encode($this->results)],
-                ]);
+        $json = json_encode(['tracks' => json_encode($tracks)]);
 
-                echo "Results sent to $endpoint: " . $response->getBody() . PHP_EOL;
-            } catch (\Exception $e) {
-                // Handle exceptions, log errors, etc.
-                echo "Error sending results to $endpoint: " . $e->getMessage() . PHP_EOL;
-            }
+        foreach ($endpoints as $endpoint) {
+            $response = $client->post($endpoint, [
+                'json' => $json,
+                'X-Signature' => hash_hmac('sha256', $json, $signingSecret),
+            ]);
         }
     }
 }
