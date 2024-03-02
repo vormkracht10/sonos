c330f6e00deaf156a1d0c564d44c3160bbc28d11
MHortulanus <manoj@vormkracht10.nl>
2024-03-02 15:02:53 +0100

    wip

--- Pause.php
+++ Pause.php
@@ -2,6 +2,7 @@
 
 use duncan3dc\Sonos\Network;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Support\Carbon;
 
 class Pause
@@ -18,7 +19,7 @@ class Pause
     {
         $network = new Network();
         $this->controllers = $network->getControllers();
-        $this->endpoint = getenv('SONOS_ENDPOINT').'/webhooks/sonos/state';
+        $this->endpoint = getenv('SONOS_ENDPOINT') . '/webhooks/sonos/state';
     }
 
     public function run()
@@ -32,7 +33,6 @@ class Pause
                 if ($controller_name === $speaker) {
                     $inputDate = $input['date'];
                     if ($inputDate && (Carbon::parse($inputDate)->gt(Carbon::now()->subSeconds(10)))) {
-
                         if ($input['state'] === 'PAUSED_PLAYBACK') {
                             echo 'Pausing';
                             $controller->pause();
@@ -54,14 +54,23 @@ class Pause
 
     public function getInfo()
     {
-        $client = new Client();
-        $request = new \GuzzleHttp\Psr7\Request('GET', $this->endpoint);
-        $result = null;
-        $promise = $client->sendAsync($request)->then(function ($response) use (&$result) {
-            $result = $response->getBody();
-        });
-        $promise->wait();
+        try {
+            $client = new Client();
+            $request = new \GuzzleHttp\Psr7\Request('GET', $this->endpoint);
+            $result = null;
+            $promise = $client->sendAsync($request)->then(function ($response) use (&$result) {
+                $result = $response->getBody();
+            });
+            $promise->wait();
 
-        return $result;
+            return $result;
+        } catch (GuzzleException $e) {
+            $info["Boiler Room"] = [
+                'state' => "PLAYING",
+                'date' => Carbon::now(),
+            ];
+            $catch = json_encode($info);
+            return $catch;
+        }
     }
 }
