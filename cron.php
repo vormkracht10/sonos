93d1fe5942bbccef6998485005e45fb754509095
MHortulanus <manoj@vormkracht10.nl>
2024-03-16 14:55:27 +0100

    wip

--- cron.php
+++ cron.php
@@ -5,12 +5,8 @@ require __DIR__ . '/bootstrap.php';
 $sonosData = new SonosData();
 $sonosPause = new Pause();
 
-foreach (range(0, 1000000) as $seconds) {
-    // $sonosData->run();
-    // try {
+foreach (range(0, 60) as $seconds) {
+    $sonosData->run();
     $sonosPause->run();
-    // } catch (Exception $e) {
-    //     echo "$e";
-    // }
     sleep(1);
 }
