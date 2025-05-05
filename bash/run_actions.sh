d88568a147d46e9957c5f8e48114b1dca76a27a2
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-05-05 10:13:10 +0200

    wip

--- bash/run_actions.sh
+++ bash/run_actions.sh
@@ -3,6 +3,6 @@
 # Run the desired command every second
 while true; do
     echo "Running actions_cron.php"
-    php ../action_cron.php
+    php "/home/vormkracht10/VK10/sonos-v1/action_cron.php"
     sleep 1
 done
--- bash/run_collection.sh
+++ bash/run_collection.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Run the desired command every second
+
 while true; do
     echo "Running collect_cron.php"
-    php ../collect_cron.php
+    php "/home/vormkracht10/VK10/sonos-v1/collect_cron.php"
     sleep 1
 done
